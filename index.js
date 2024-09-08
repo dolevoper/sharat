2a5f6fb748302e5dd2b3c8ec8416862d4f26cd63
dolevoper <omerdolev90@gmail.com>
2024-09-08 23:36:14 +0300

    basic ts support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import * as path from "node:path";
 import * as fs from "node:fs/promises";
 import { getPortPromise } from "portfinder";
 import mime from "mime";
+import * as ts from "typescript";
 
 const server = createServer(async (req, res) => {
     console.log(req.method, req.url);
@@ -12,10 +13,26 @@ const server = createServer(async (req, res) => {
     try {
         const fileContents = await getFileContents(filePath);
 
+        if (filePath.endsWith(".ts")) {
+            res.writeHead(200, { "Content-Type": "text/javascript" });
+
+            const configPath = ts.findConfigFile(process.cwd(), ts.sys.fileExists, "tsconfig.json");
+            const configFile = configPath !== undefined ? ts.readConfigFile(configPath, ts.sys.readFile).config : {};
+
+            res.end(ts.transpileModule(fileContents.toString(), { compilerOptions: configFile.compilerOptions }).outputText);
+
+            return;
+        }
+
         res.writeHead(200, { "Content-Type": mime.getType(filePath) ?? "text/html" });
         res.end(fileContents);
     } catch (error) {
-        res.writeHead(404, { "Content-Type": "text/html" });
+        console.error(error);
+
+        if (!res.headersSent) {
+            res.writeHead(404, { "Content-Type": "text/html" });
+        }
+
         res.end("<h1>Not found</h1>");
     }
 });

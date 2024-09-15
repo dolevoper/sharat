e79a3d16116da8f7e7bf21b25d97d546a6537fb3
dolevoper <omerdolev90@gmail.com>
2024-09-15 07:32:41 +0300

    support scss and improve logging

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import * as fs from "node:fs/promises";
 import { getPortPromise } from "portfinder";
 import mime from "mime";
 import * as ts from "typescript";
+import * as sass from "sass";
 
 const cwd = process.cwd();
 
 const server = createServer(async (req, res) => {
-    console.log(req.method, req.url);
+    console.log("->", req.method, req.url);
 
     try {
         for (const contentsGetter of [getFileContents, getDirectoryContents, getTSContents, getJSContents]) {
@@ -19,27 +20,49 @@ const server = createServer(async (req, res) => {
             }
 
             if (contents.extension === ".ts") {
-                res.writeHead(200, { "Content-Type": "text/javascript" });
-
                 const configPath = ts.findConfigFile(process.cwd(), ts.sys.fileExists, "tsconfig.json");
                 const configFile = configPath !== undefined ? ts.readConfigFile(configPath, ts.sys.readFile).config : {};
-
-                res.end(ts.transpileModule(contents.data.toString(), { fileName: contents.fileName, compilerOptions: configFile.compilerOptions }).outputText);
+                const data = ts.transpileModule(contents.data.toString(), { fileName: contents.fileName, compilerOptions: configFile.compilerOptions }).outputText;
+                
+                console.log("<-", req.method, req.url, 200);
+                res.writeHead(200, { "Content-Type": "text/javascript" });
+                res.end(data);
 
                 return;
             }
 
+            if (contents.extension === ".scss") {
+                const data = (await sass.compileStringAsync(contents.data.toString())).css;
+
+                console.log("<-", req.method, req.url, 200);
+                res.writeHead(200, { "Content-Type": "text/css" });
+                res.end(data);
+
+                return;
+            }
+
+            console.log("<-", req.method, req.url, 200);
             res.writeHead(200, { "Content-Type": mime.getType(contents.extension) ?? "text/html" });
             res.end(contents.data);
 
             return;
         }
 
+        if (res.headersSent) {
+            return;
+        }
+
+        console.log("<-", req.method, req.url, 404);
         res.writeHead(404, { "Content-Type": "text/html" });
         res.end("<h1>Not found</h1>");
     } catch (error) {
         console.error(error);
-
+        
+        if (res.headersSent) {
+            return;
+        }
+        
+        console.log("<-", req.method, req.url, 500);
         res.writeHead(500, { "Content-Type": "text/html" });
         res.end("<h1>Internal server error</h1>");
     }

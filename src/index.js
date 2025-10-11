57f39d7ff078414f19b6b811029e1cc7120708b1
dolevoper <omerdolev90@gmail.com>
2025-10-12 00:52:36 +0300

    wip

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@
 
 import "./dotenv.js";
 
-// import * as path from "node:path";
-// import * as fs from "node:fs/promises";
-// import { readFileSync } from "node:fs";
 import { getPortPromise } from "portfinder";
-// import mime from "mime";
-// import * as ts from "typescript";
-// import * as sass from "sass";
 import { server } from "./server.js";
 import * as logger from "./logger.js";
 

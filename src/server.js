7405bb74b40a614e08fa96a1fb6e142ae9597bbd
dolevoper <omerdolev90@gmail.com>
2025-10-11 22:18:41 +0300

    server basic structure

--- src/server.js
+++ src/server.js
@@ -1,3 +1,26 @@
 import { createServer } from "node:http";
+import * as logger from "./logger.js";
 
-export const server = createServer(async function (req, res) {});
+export const server = createServer(async function (req, res) {
+    const { pathname } = new URL(`http://localhost${req.url}`);
+    const method = req.method;
+
+    switch (true) {
+        case method === "GET" && pathname === "/__sharat_events__.js":
+            logger.debug("<-", "\x1b[32m", method, "\x1b[0m", req.url);
+            // return sharatEvents.js
+            break;
+        case method === "GET" && pathname === "/__sharat_events__":
+            logger.debug("<-", "\x1b[32m", method, "\x1b[0m", req.url);
+            // register event listener
+            break;
+        case method === "GET":
+            // handle getting file from file system
+            logger.info("<-", "\x1b[32m", method, "\x1b[0m", req.url);
+            break;
+        default:
+            logger.error(`Unsupported request: ${method} ${req.url}`);
+
+        res.end();
+    }
+});

137a6e9af7973eefa466eefadbcfac67b6f64de4
dolevoper <omerdolev90@gmail.com>
2025-10-11 22:36:04 +0300

    basic implementation

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 import { createServer } from "node:http";
+import * as path from "node:path";
+import * as fs from "node:fs/promises";
+import mime from "mime";
 import * as logger from "./logger.js";
 
 export const server = createServer(async function (req, res) {
@@ -15,12 +18,41 @@ export const server = createServer(async function (req, res) {
             // register event listener
             break;
         case method === "GET":
-            // handle getting file from file system
             logger.info("<-", "\x1b[32m", method, "\x1b[0m", req.url);
+            const filePath = path.join(process.cwd(), pathname);
+
+            try {
+                const data = await fs.readFile(filePath);
+
+                if (res.headersSent) {
+                    return;
+                }
+
+                const extension = path.extname(filePath);
+                const contentType = mime.getType(extension);
+
+                logger.info("->", "\x1b[32m", method, "\x1b[0m", req.url, "-", pathname, "(", contentType, ")");
+                res.writeHead(200, { "content-type": contentType });
+                res.end(data);
+            } catch (err) {
+                if (err.code !== "ENOENT" && err.code !== "EISDIR") {
+                    logger.error(err);
+                    logger.info("->", "\x1b[32m", method, "\x1b[0m", req.url, "-", "\x1b[31m500\x1b[0m");
+                    res.writeHead(500);
+                    res.end();
+
+                    return;
+                }
+
+                logger.info("->", "\x1b[32m", method, "\x1b[0m", req.url, "-", "\x1b[33m404\x1b[0m");
+                res.writeHead(404);
+                res.end();
+            }
+
             break;
         default:
             logger.error(`Unsupported request: ${method} ${req.url}`);
 
-        res.end();
+            res.end();
     }
 });

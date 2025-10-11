83f95fbabaeac9ccac0136864ab9d11464c21b24
dolevoper <omerdolev90@gmail.com>
2025-10-12 00:51:20 +0300

    cache

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import * as logger from "./logger.js";
 import * as contentLoader from "./contentLoader.js";
 import * as transformer from "./transformer.js";
 
+const cache = new Map();
+
 export const server = createServer(async function (req, res) {
     const { pathname } = new URL(`http://localhost${req.url}`);
     const method = req.method;
@@ -19,6 +21,17 @@ export const server = createServer(async function (req, res) {
             break;
         case method === "GET":
             logger.info("<-", "\x1b[32m", method, "\x1b[0m", req.url);
+
+            if (cache.has(req.url)) {
+                const content = cache.get(req.url);
+                const contentType = content.contentType ?? mime.getType(content.extension);
+
+                logger.info("->", "\x1b[32m", method, "\x1b[0m", req.url, "-", content.filePath, `(${contentType})`, "[FROM CACHE]");
+                res.writeHead(200, { "content-type": contentType });
+                res.end(content.data);
+
+                return;
+            }
 
             try {
                 const content = await contentLoader.load(pathname);
@@ -37,10 +50,12 @@ export const server = createServer(async function (req, res) {
 
                 const transformedContent = await transformer.transform(content);
 
+                cache.set(req.url, transformedContent);
+
                 if (res.headersSent) {
                     return;
                 }
-                
+
                 const contentType = transformedContent.contentType ?? mime.getType(transformedContent.extension);
 
                 logger.info("->", "\x1b[32m", method, "\x1b[0m", req.url, "-", transformedContent.filePath, `(${contentType})`);
